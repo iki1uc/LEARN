5a6927e7380ba3cfce0447e2fad8808b600cd0c3
iki1uc <entertrainer@web.de>
2026-08-27 16:57:36 +0200

    Delete use directory

--- use/index.js
+++
@@ -1,9 +0,0 @@
-import { ORT } from "../router.js";
-
-(async () => {
-    const user = await ORT.loadUser();
-
-    document.getElementById("user-name").innerText = user?.name ?? "Gast";
-    document.getElementById("user-level").innerText = user?.level ?? "-";
-    document.getElementById("user-status").innerText = user?.status ?? "unknown";
-})();
